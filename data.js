f42df17de295aa2f666ac2bebb333024b8e9335f
Sashapotkin <35652550+Sashapotkin@users.noreply.github.com>
2018-01-21 12:18:34 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth() + 1;
+    var year = d.getFullYear();
+    var name_input = document.getElementById('your_name')
+    name_input.value = day + "-" + month + "-" + year;
+});
